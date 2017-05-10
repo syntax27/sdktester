fbbbdd9b9fd7fac4831a62363d0691a61f883daf
Andrea Bondi <andrea@andreabondi.it>
2017-05-10 09:35:34 +0100

    Update some comments

--- SDKtester/PYPLCheckoutBridge.h
+++ SDKtester/PYPLCheckoutBridge.h
@@ -4,13 +4,15 @@
 //  Copyright © 2016 PayPal. All rights reserved.
 //
 
+// Bridge for NativeXO SDK
 #ifndef PYPLCheckoutBridge_h
 #define PYPLCheckoutBridge_h
 
 #import "PYPLCheckout.h"
 
-#endif /* PYPLCheckoutBridge_h */
+#endif
 
+// Bridge for Popup Bridge SDK
 #ifndef POPPopupUpBridgeBridge_h
 #define POPPopupUpBridgeBridge_h
 
